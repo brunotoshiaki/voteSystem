a7f5fb01beda666ebd4d55c3d097c426150898f2
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2023-01-03 17:00:36 -0300

    Criando camada de modelo

---
+++ src/main/java/br/com/bruno/system/core/domain/Associate.java
@@ -0,0 +1,5 @@
+package br.com.bruno.system.core.domain;
+
+public record Associate(String name) {
+
+}
---
+++ src/main/java/br/com/bruno/system/core/domain/Schedule.java
@@ -0,0 +1,5 @@
+package br.com.bruno.system.core.domain;
+
+public record Schedule(Associate associate,  String name) {
+
+}
