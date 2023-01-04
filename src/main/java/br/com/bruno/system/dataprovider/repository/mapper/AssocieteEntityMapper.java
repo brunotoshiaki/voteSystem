f20c2417c1577a073d553361ebede1dd1af73b15
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2023-01-04 14:49:46 -0300

    Ajuste na criação de associados

--- src/main/java/br/com/bruno/system/dataprovider/repository/mapper/AssocieteEntityMapper.java
+++ src/main/java/br/com/bruno/system/dataprovider/repository/mapper/AssocieteEntityMapper.java
@@ -3,6 +3,7 @@ package br.com.bruno.system.dataprovider.repository.mapper;
 import br.com.bruno.system.core.domain.Associate;
 import br.com.bruno.system.dataprovider.repository.entity.AssociateEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper()
@@ -10,6 +11,7 @@ public interface AssocieteEntityMapper {
 
   AssocieteEntityMapper INSTANCE = Mappers.getMapper(AssocieteEntityMapper.class);
 
+  @Mapping(target = "id", ignore = true)
   AssociateEntity toAssociateEntity(final Associate associate);
 
 
